9607ad6e01b4b0c4a459b54a268833dcfe735bd5
tsamantanis <philip.tsamantanis@icloud.com>
2021-05-03 16:14:01 -0700

    Create homepage route and template using dummy data

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,5 +1,29 @@
-from flask import Blueprint
+from flask import Blueprint, request, redirect, render_template, url_for
+from datetime import date, datetime, timedelta
+import calendar
 
 main = Blueprint('main', __name__)
 
-# Create your routes here.
+@main.route('/')
+def home():
+    """Display all events and users in kanban calendar."""
+    return redirect(url_for('main.get_calendar', date_input = datetime.now().strftime('%Y-%m-%d')))
+
+@main.route('/get_calendar/<date_input>')
+def get_calendar(date_input):
+    """Returns current state of the calendar"""
+    month_range = calendar.monthrange(int(date_input[0:4]), int(date_input[5:7]))
+    prev_month_range = calendar.monthrange(int(date_input[0:4]), int(date_input[5:7]) - 1)
+    date_current = date(int(date_input[0:4]), int(date_input[5:7]), int(date_input[8:10]))
+    date_next = date(int(date_input[0:4]), int(date_input[5:7]), int(date_input[8:10]) + 1 if int(date_input[8:10]) < month_range[1] else 1)
+    date_prev = date(int(date_input[0:4]), int(date_input[5:7]), int(date_input[8:10]) - 1 if int(date_input[8:10]) > 1 else prev_month_range[1])
+    context = {
+        "timeslots": [], # timeslots,
+        "is_empty": True, # len(user_list) == 0,
+        "user_list": [], # user_list,
+        "date": str(date_current.month) + "/" + str(date_current.day),
+        "date_full": date_input,
+        "date_next": date_next.strftime('%Y-%m-%d'),
+        "date_prev": date_prev.strftime('%Y-%m-%d')
+    }
+    return render_template('calendar.html', **context)
