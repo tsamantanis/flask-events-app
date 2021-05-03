a7c02dad6e7bf085791d8055e4427d420ec5e031
tsamantanis <philip.tsamantanis@icloud.com>
2021-05-03 14:27:44 -0700

    Create sql models

--- app/models.py
+++ app/models.py
@@ -1,1 +1,31 @@
-# Create your models here.
+from sqlalchemy_utils import URLType
+
+from grocery_app import db
+from grocery_app.utils import FormEnum
+from flask_login import UserMixin
+
+class Event(db.Model):
+    """Event model."""
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    user = db.relationship('User', back_populates='events')
+    equipment = db.relationship('Equipment', back_populates='event')
+    created_by_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    created_by = db.relationship("User")
+
+class Equipment(db.Model):
+    """Equipment model."""
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    event = db.relationship('User', back_populates='event')
+
+class User(UserMixin, db.Model):
+    """User model"""
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(80), nullable = False, unique = True)
+    password = db.Column(db.String(120), nullable = False)
+    events = db.relationship('Event', back_populates='user')
+
+    def __repr__(self):
+        return f"<User: {self.username}>"
