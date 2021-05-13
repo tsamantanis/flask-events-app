123238977ad21a88f95ddec66ac8f99c64cd2f0a
tsamantanis <philip.tsamantanis@icloud.com>
2021-05-13 10:20:27 -0700

    Create 404 page

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 from app import app
+from flask import render_template
+
+@app.errorhandler(404)
+def show_404(error):
+    """Display a 404 error page"""
+    return render_template('error_page.html', message = "This page has melted in the sun", button = "Back to Home"), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
